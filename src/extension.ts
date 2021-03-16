f7a7983afd2292fc8b53a74fc2d1a46456cc89d8
parmigggiana <francesco.basile12@studenti.unina.it>
2021-03-16 23:17:07 +0100

    fix?

--- src/extension.ts
+++ src/extension.ts
@@ -13,12 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	//let disposable = vscode.commands.registerCommand('cpp-snip.initialize', () => {
+	let disposable = vscode.commands.registerCommand('cpp-snip.startsnip', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		//vscode.window.showInformationMessage('AutoInclude has been activated!');
-	//});
+	});
 
 	//context.subscriptions.push(disposable);
 }
